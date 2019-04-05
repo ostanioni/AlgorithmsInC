fd9c7fc7ac344bb5ccd2b4bc22210abea767d85d
ostanioni <ostanioni@gmail.com>
2019-04-05 11:01:46 +0700

    test info added

--- src/1_Fundamentals/pr_2.c
+++ src/1_Fundamentals/pr_2.c
@@ -5,7 +5,8 @@ int heads()
 int main(int argc, char *argv[])
   { int i, j, cnt;
     int N = atoi(argv[1]), M = atoi(argv[2]);
-    int *f =(int*)malloc((N+1)*sizeof(int));
+    printf("first=%d, second=%d", N, M);
+    int *f =(int*) malloc((N+1)*sizeof(int));
     for (j = 0; j <= N; j++) f[j] = 0;
     for (i = 0; i < M; i++, f[cnt]++)
       for (cnt = 0, j = 0; j <= N; j++) 
